afde5255662c5166c98b75bfd5bcb3ab4a7938da
李添财 <narci.ltc@toceansoft.com>
2017-12-25 18:00:26 +0800

    local

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 source /etc/profile
 
-MYIMAGE=192.168.1.2:8082/springboot/springboot-jpa
+MYIMAGE=192.168.3.188:8082/springboot/springboot-jpa
 
 # uncomment if you need push
-#docker login 192.168.1.2:8082 -u admin -p admin123
+#docker login 192.168.3.188:8082 -u admin -p admin123
 
 # stop all container
 docker kill $(docker ps -aq)
@@ -14,7 +14,7 @@ docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 
 # remove old images
-docker images | grep 192.168.1.2:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
+docker images | grep 192.168.3.188:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
 
 # build jar and image
 mvn package -e -X docker:build -DskipTest
